2bc810930d91c78237513d0cb16f9f00e040e260
ivanvb07 <168553347+ivanvb07@users.noreply.github.com>
2024-05-14 13:28:52 +0200

    Update Division.java

--- Division.java
+++ Division.java
@@ -12,9 +12,9 @@ public class Division extends Operation{ //initialisation de la classe Multiplic
         super(d); //appel du constructeur par copie de la classe Operation
     }
   
-    public int valeur() throws Exception{ //implémentation de la méthode abstraite valeur de la classe Operation
-        if (this.getOPerande1()==0 || this.getOPerande2==0 )
-            throw (new ArithmeticException());
+    public int valeur() throws ArithmeticException{ //implémentation de la méthode abstraite valeur de la classe Operation en précisant qu'elle peut déclencher une exception (division par 0)
+        if (this.getOPerande1()==0 || this.getOPerande2==0 ) //vérifie si l'une des deux opérandes vaut 0 afin de traiter la division par zéro
+            throw (new ArithmeticException()); //déclenche une exception de type arithmétique qui sera traitée par le main de la classe CalculatriceSimple
         return this.getOPerande1()/this.getOPerande2(); //renvoie le résultat de la division des deux opérandes de l'objet courant (this)
     }
 

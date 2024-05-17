de9be454ca7baab2a72d61deca716c2029f6e240
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-17 13:37:48 +0200

    Update CalculatriceSimple.java

--- CalculatriceSimple.java
+++ CalculatriceSimple.java
@@ -11,13 +11,15 @@ public class CalculatriceSimple {
         System.out.println(n2);
 
         Operation o1 = new Addition(n1,n2);
+        Addition a=(Operation)o1;
         System.out.println(o1);
 
         Operation o2 = new Soustraction(n1,n2);
+        Soustraction s=(Operation)o2;
         System.out.println(o2);
 
         Operation o3 = new Multiplication(n1,n2);
-        System.out.println(o1);
+        System.out.println(o3);
 
         try {
             Operation o4 = new Division(n1,n2);

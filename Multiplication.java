154f3a913c4251373674836a30638c1468f49beb
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-18 22:35:12 +0200

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,25 +1,25 @@
-public class Multiplication extends Operation { //initialisation de la classe Multiplication héritant d'Operation
+public class Multiplication extends Operation { // initialisation de la classe Multiplication héritant d'Operation
 
-    public Multiplication() { //constructeur par défaut
-        super(); //appel le constructeur par défaut de Operation
+    public Multiplication() { // constructeur par défaut
+        super(); // appel le constructeur par défaut de Operation
     }
 
-    public Multiplication(Nombre nb1, Nombre nb2){ //constructeur champ à champ
-        super(nb1,nb2); //appel du constructeur champ à champ de Operation
+    public Multiplication(Nombre nb1, Nombre nb2) { // constructeur champ à champ
+        super(nb1, nb2); // appel du constructeur champ à champ de Operation
     }
 
-    public Multiplication(Multiplicationm) { //constructeur par copie
-        super(m); //appel du constructeur par copie de Operation
-    }
-  
-    public int valeur(){ //implémentation de la méthode abstraite valeur() de Operation
-        return this.getOPerande1() * this.getOPerande2(); //retourne le produit des deux opérandes
+    public Multiplication(Multiplication m) { // constructeur par copie
+        super(m); // appel du constructeur par copie de Operation
     }
 
-    public String toString(){ //méthode toString
-        String ch="Résultat de "+ this.getOPerande1() +" x " + 
-        this.getOPerande2() + " : ";
-        ch+=this.valeur();
+    public int valeur() { // implémentation de la méthode abstraite valeur() de Operation
+        return this.getOPerande1().valeur() * this.getOPerande2().valeur(); // retourne le produit des valeurs des deux opérandes
+    }
+
+    public String toString() { // méthode toString
+        String ch = "Résultat de " + this.getOPerande1().valeur() + " x " +
+                this.getOPerande2().valeur() + " : ";
+        ch += this.valeur();
         return ch;
     }
 }

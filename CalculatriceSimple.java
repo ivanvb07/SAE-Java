13e95db953c4eef7660399e72bd2f8005312044f
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-18 22:21:54 +0200

    Update CalculatriceSimple.java

--- CalculatriceSimple.java
+++ CalculatriceSimple.java
@@ -1,33 +1,35 @@
-import java.util.*;
-
 public class CalculatriceSimple {
 
     public static void main(String[] args) {
 
-        Nombre n1 = new Nombre(4);
+        Nombre n1 = new Nombre(5);
         System.out.println(n1);
 
-        Nombre n2 = new Nombre(0);
+        Nombre n2 = new Nombre(3);
         System.out.println(n2);
 
-        Operation o1 = new Addition(n1,n2);
-        Addition a=(Operation)o1;
+        Nombre n3 = new Nombre(0);
+        System.out.println(n3);
+
+        Operation o1 = new Addition(n1, n2);
         System.out.println(o1);
 
-        Operation o2 = new Soustraction(n1,n2);
-        Soustraction s=(Operation)o2;
+        Operation o2 = new Soustraction(n1, n2);
         System.out.println(o2);
 
-        Operation o3 = new Multiplication(n1,n2);
+        Operation o3 = new Multiplication(n1, n2);
         System.out.println(o3);
 
+        Operation o4 = new Division(n1, n2);
+        System.out.println(o4);
+
         try {
-            Operation o4 = new Division(n1,n2);
-            System.out.println(o4);
+            Operation o5 = new Division(n1, n3);
+            System.out.println(o5);
         }
 
-        catch(ArithmeticException e){
-            System.out.println("Erreur : divison par zéro" + e);
+        catch (ArithmeticException e) {
+            System.out.println("Erreur " + e + " : division par zéro");
         }
     }
 }

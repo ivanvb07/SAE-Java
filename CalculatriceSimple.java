7a88aa7e847e46b50f120087fee94e73d9894faa
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-18 22:51:58 +0200

    Update CalculatriceSimple.java

--- CalculatriceSimple.java
+++ CalculatriceSimple.java
@@ -2,28 +2,28 @@ public class CalculatriceSimple { //initialisation de la classe test
 
     public static void main(String[] args) { //méthode main
 
-        Nombre n1 = new Nombre(5); //création de l'objet n1 de type Nombre avec comme valeurNombre 5 (appel du constructeur champ-à-champ de Nombre)
-        System.out.println(n1); //appel de la méthode toString de la classe Nombre
-
-        Nombre n2 = new Nombre(3); //création de l'objet n2 de type Nombre avec comme valeurNombre 3 (appel du constructeur champ-à-champ de Nombre)
-        System.out.println(n2); //appel de la méthode toString de la classe Nombre
-
-        Nombre n3 = new Nombre(0); //création de l'objet n3 de type Nombre avec comme valeurNombre 0 (appel du constructeur champ-à-champ de Nombre)
-        System.out.println(n3); //appel de la méthode toString de la classe Nombre
-
-        Operation o1 = new Addition(n1, n2); //création de l'objet o1 de type Operation (qu'on pourrait aussi mettre de type Addition)
-        System.out.println(o1); //appel de la méthode toString de la classe Addition
-
-        Operation o2 = new Soustraction(n1, n2); //création de l'objet o2 de type Operation (qu'on pourrait aussi mettre de type Soustraction)
-        System.out.println(o2); //appel de la méthode toString de la classe Soustraction
-
-        Operation o3 = new Multiplication(n1, n2); //création de l'objet o3 de type Operation (qu'on pourrait aussi mettre de type Multiplication)
-        System.out.println(o3); //appel de la méthode toString de la classe Multiplication
-
-        Operation o4 = new Division(n1, n2); //création de l'objet o4 de type Operation (qu'on pourrait aussi mettre de type Division)
-        System.out.println(o4); //appel de la méthode toString de la classe Division
-
         try { //on essaie d'exécuter les instructions du bloc try tant qu'aucune erreur/exception n'est détectée
+            Nombre n1 = new Nombre(5); //création de l'objet n1 de type Nombre avec comme valeurNombre 5 (appel du constructeur champ-à-champ de Nombre)
+            System.out.println(n1); //appel de la méthode toString de la classe Nombre
+    
+            Nombre n2 = new Nombre(3); //création de l'objet n2 de type Nombre avec comme valeurNombre 3 (appel du constructeur champ-à-champ de Nombre)
+            System.out.println(n2); //appel de la méthode toString de la classe Nombre
+    
+            Nombre n3 = new Nombre(0); //création de l'objet n3 de type Nombre avec comme valeurNombre 0 (appel du constructeur champ-à-champ de Nombre)
+            System.out.println(n3); //appel de la méthode toString de la classe Nombre
+    
+            Operation o1 = new Addition(n1, n2); //création de l'objet o1 de type Operation (qu'on pourrait aussi mettre de type Addition)
+            System.out.println(o1); //appel de la méthode toString de la classe Addition
+    
+            Operation o2 = new Soustraction(n1, n2); //création de l'objet o2 de type Operation (qu'on pourrait aussi mettre de type Soustraction)
+            System.out.println(o2); //appel de la méthode toString de la classe Soustraction
+    
+            Operation o3 = new Multiplication(n1, n2); //création de l'objet o3 de type Operation (qu'on pourrait aussi mettre de type Multiplication)
+            System.out.println(o3); //appel de la méthode toString de la classe Multiplication
+    
+            Operation o4 = new Division(n1, n2); //création de l'objet o4 de type Operation (qu'on pourrait aussi mettre de type Division)
+            System.out.println(o4); //appel de la méthode toString de la classe Division
+
             Operation o5 = new Division(n1, n3); //création de l'objet o5 de type Operation ((qu'on pourrait aussi mettre de type Division)
             System.out.println(o5); //appel de la méthode toString de la classe Division
         }

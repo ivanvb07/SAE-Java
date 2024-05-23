f305a8b9f4c992d8b7aad974760136d81cd129cb
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-23 13:13:04 +0200

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,6 +1,6 @@
-public class abstract Expression{
-  
+public abstract class Expression{
+
   public double valeur(){
-    
+    return this.getOperande1().valeur() this.getOperande2().valeur();
   }
 }

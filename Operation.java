b6967048596db4d63f9de67793903d143df12aa1
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-23 13:01:54 +0200

    Update Operation.java

--- Operation.java
+++ Operation.java
@@ -1,31 +1,31 @@
 public abstract class Operation { // création de la classe abstraite Operation
 
-    private Nombre nombre1; // attribut nombre 1 de type nombre
-    private Nombre nombre2; // attribut nombre 2 de type nombre
+    private Expression exp1; // attribut exp1 de type Expression
+    private Expression exp2; // attribut exp2 de type Expression
 
     public Operation() { // constructeur par défaut
-        this.nombre1 = new Nombre(); // appel du constructeur par défaut de la classe Nombre qui donnera la valeur 0
-        this.nombre2 = new Nombre(); // appel du constructeur par défaut de la classe Nombre qui donnera la valeur 0
+        this.exp1 = new Nombre(); // appel du constructeur par défaut de la classe Expression qui donnera la valeur 0
+        this.exp2 = new Nombre(); // appel du constructeur par défaut de la classe Expression qui donnera la valeur 0
     }
 
-    public Operation(Nombre nb1, Nombre nb2) { // constructeur champ à champ
-        this.nombre1 = nb1; // récupère la valeur nb1 passée en paramètre
-        this.nombre2 = nb2; // récupère la valeur nb2 passée en paramètre
+    public Operation(Expression ex1, Expression ex2) { // constructeur champ à champ
+        this.exp1 = ex1; // récupère la valeur ex1 passée en paramètre
+        this.exp2 = ex2; // récupère la valeur ex2 passée en paramètre
     }
 
     public Operation(Operation o) { // constructeur par copie
-        this.nombre1 = o.getOPerande1(); // récupère la première Operande de type Nombre de l'Objet o
-        this.nombre2 = o.getOPerande2(); // récupère la deuxième Operande de type Nombre de l'Objet o
+        this.exp1 = o.getOPerande1(); // récupère la première Operande de type Expression de l'Objet o
+        this.exp2 = o.getOPerande2(); // récupère la deuxième Operande de type Expression de l'Objet o
     }
 
     public abstract int valeur(); // définition de la méthode abstraite valeur() qui sera implémentée dans les sous-classes
 
-    public Nombre getOPerande1() { // définition de la méthode getOPerande1() qui renvoie un Objet de type Nombre
-        return this.nombre1; // renvoie la première Operande de type Nombre de l'Objet courant
+    public Expression getOPerande1() { // définition de la méthode getOPerande1() qui renvoie un Objet de type Expression
+        return this.nombre1; // renvoie la première Operande de type Expression de l'Objet courant
     }
 
-    public Nombre getOPerande2() { // définition de la méthode getOPerande2() qui renvoie un Objet de type Nombre
-        return this.nombre2; // renvoie la deuxième Operande de type Nombre l'Objet courant
+    public Expression getOPerande2() { // définition de la méthode getOPerande2() qui renvoie un Objet de type Expression
+        return this.nombre2; // renvoie la deuxième Operande de type Expression l'Objet courant
     }
 
 }

fa8931ab74ae463c4fdb3d73d50369e7145b6c79
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-30 10:56:44 +0200

    Update Calculatrice.java

--- Calculatrice.java
+++ Calculatrice.java
@@ -4,23 +4,23 @@ public class Calculatrice { //initialisation de la classe test
 
         try { //on essaie d'exécuter les instructions du bloc try tant qu'aucune erreur/exception n'est détectée
             
-	          Expression deux = new Nombre(2) ;
-	          System.out.println(deux);
+	    Expression deux = new Nombre(2) ; //création d'un objet de type Expression avec le constructeur de la classe Nombre
+	    System.out.println(deux); //appel de la méthode toString de la classe Nombre
 		
-	          Expression trois = new Nombre(3) ;
-	          System.out.println(trois);
+	    Expression trois = new Nombre(3) ; //création d'un objet de type Expression avec le constructeur de la classe Nombre
+	    System.out.println(trois); //appel de la méthode toString de la classe Nombre
 
-	          Expression dixSept = new Nombre(17) ;
-	          System.out.println(dixSept);
+	    Expression dixSept = new Nombre(17) ; //création d'un objet de type Expression avec le constructeur de la classe Nombre
+            System.out.println(dixSept); //appel de la méthode toString de la classe Nombre
 
-	          Expression s = new Soustraction(dixSept, deux) ;
-	          System.out.println(s);
+	    Expression s = new Soustraction(dixSept, deux) ; //création d'un objet de type Expression avec le constructeur de la classe Soustraction
+	    System.out.println(s); //appel de la méthode toString de la classe Soustraction
 
-	          Expression a = new Addition(deux, trois) ;
-	          System.out.println(a);
+	    Expression a = new Addition(deux, trois) ; //création d'un objet de type Expression avec le constructeur de la classe Addition
+	    System.out.println(a);  //appel de la méthode toString de la classe Addition
 
-	          Expression d = new Division(s, a) ;
-            System.out.println(d) ;
+	    Expression d = new Division(s, a) ; //création d'un objet de type Expression avec le constructeur de la classe Division
+            System.out.println(d) ; //appel de la méthode toString de la classe Division
         }
 
         catch (ArithmeticException e) { //si une erreur/exception est détectée par le bloc try, alors catch l'attrape et exécute les instructions de son propre bloc

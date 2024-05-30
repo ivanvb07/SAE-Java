4443baf6f6168d75e3bee94a3796ce1a8d9d9153
Veljovic Bulatovic Ivan <168553347+ivanvb07@users.noreply.github.com>
2024-05-30 10:35:12 +0200

    Update Soustraction.java

--- Soustraction.java
+++ Soustraction.java
@@ -4,7 +4,7 @@ public class Soustraction extends Operation { // initialisation de la classe Sou
         super(); // appel du constructeur par défaut de Operation
     }
 
-    public Soustraction(Nombre nb1, Nombre nb2) { // constructeur champ à champ
+    public Soustraction(Expression nb1, Expression nb2) { // constructeur champ à champ
         super(nb1, nb2); // appel du constructeur champ à champ de Operation
     }
 
@@ -12,7 +12,7 @@ public class Soustraction extends Operation { // initialisation de la classe Sou
         super(s); // appel du constructeur par copie de Operation
     }
 
-    public int valeur() { // implémentation de la méthode abstraite valeur() de Operation
+    public double valeur() { // implémentation de la méthode abstraite valeur() de Expression
         return this.getOPerande1().valeur() - this.getOPerande2().valeur(); // renvoie la différence des valeurs des deux opérandes de l'objet courant (this)
     }
 
